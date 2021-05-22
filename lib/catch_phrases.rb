8b2b15a5e04a313063a2f7209fb60206e857343e
fortuitous-prototype-dfa5 <lindsay.chen@yale.edu>
2021-05-22 09:32:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,11 +5,3 @@ describe "all_phrases" do
     expect{all_phrases}.to output(/It's Dangerous To Go Alone! Take This.\n/).to_stdout
   end
 end
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
-end
